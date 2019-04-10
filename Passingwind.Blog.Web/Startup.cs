d510c3a926b0b6c0f75ba329a4cdb5f2dab7681d
jxnkwlp <jxnkwlp@hotmail.com>
2019-04-10 09:32:03 +0800

    update to asp.net core 2.2

--- Passingwind.Blog.Web/Startup.cs
+++ Passingwind.Blog.Web/Startup.cs
@@ -22,241 +22,248 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Passingwind.Blog.Web
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public IConfiguration Configuration { get; }
 
-        public IConfiguration Configuration { get; }
+		public bool UseHttps { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
 
-            // services.AddAutoRegisterService();
+			UseHttps = configuration.GetValue("UseHttps", false);
+		}
 
-            services.AddDbContextPool<BlogDbContext>(options =>
-            {
-                var dbType = Configuration.GetValue<string>("DbType");
-                if (string.Equals(dbType, "sqlite", StringComparison.OrdinalIgnoreCase))
-                {
-                    options.UseSqlite(Configuration.GetConnectionString("Sqlite"));
-                }
-                else if (string.Equals(dbType, "sqlserver", StringComparison.OrdinalIgnoreCase))
-                {
-                    options.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
-                }
-                else
-                {
-                    throw new NotSupportedException("Unknow the databse type !");
-                }
-            }) // default Scoped ServiceLifetime
-            .AddScoped<DbContext, BlogDbContext>()
-            // .AddScoped<DbInitializer>()
-            .AddScoped<EntityStore>();
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.Configure<CookiePolicyOptions>(options =>
+			{
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.CheckConsentNeeded = context => true;
+				options.MinimumSameSitePolicy = SameSiteMode.None;
+			});
 
-            services.AddIdentity<User, Role>(options =>
-            {
-                // set password rule 
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireDigit = true;
-                options.Password.RequireUppercase = false;
-                options.Password.RequireLowercase = false;
+			// services.AddAutoRegisterService();
 
-            })
-            .AddUserManager<UserManager>()
-            .AddRoleManager<RoleManager>()
-            .AddSignInManager<SignInManager>()
-            .AddEntityFrameworkStores<DbContext>()
-            .AddDefaultTokenProviders();
+			services.AddDbContextPool<BlogDbContext>(options =>
+			{
+				var dbType = Configuration.GetValue<string>("DbType");
+				if (string.Equals(dbType, "sqlite", StringComparison.OrdinalIgnoreCase))
+				{
+					options.UseSqlite(Configuration.GetConnectionString("Sqlite"));
+				}
+				else if (string.Equals(dbType, "sqlserver", StringComparison.OrdinalIgnoreCase))
+				{
+					options.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
+				}
+				else
+				{
+					throw new NotSupportedException("Unknow the databse type !");
+				}
+			}) // default Scoped ServiceLifetime
+			.AddScoped<DbContext, BlogDbContext>()
+			// .AddScoped<DbInitializer>()
+			.AddScoped<EntityStore>();
 
-            // Add application services.
-            services.AddTransient<PostManager>();
-            services.AddTransient<PageManager>();
-            services.AddTransient<CommentManager>();
-            services.AddTransient<CategoryManager>();
-            services.AddTransient<TagsManager>();
-            services.AddTransient<SettingManager>();
-            services.AddTransient<IEmailSender, EmailSender>();
-            //services.AddTransient<ISmsSender, AuthMessageSender>();
-            services.AddTransient<BlogMLImporter>();
-            services.AddTransient<BlogMLExporter>();
-            services.AddTransient<CaptchaService>();
-            services.AddTransient<IFileService, LocalFileService>();
+			services.AddIdentity<User, Role>(options =>
+			{
+				// set password rule 
+				options.Password.RequireNonAlphanumeric = false;
+				options.Password.RequireDigit = true;
+				options.Password.RequireUppercase = false;
+				options.Password.RequireLowercase = false;
 
-            services.AddScoped<DbInitializer>();
+			})
+			.AddUserManager<UserManager>()
+			.AddRoleManager<RoleManager>()
+			.AddSignInManager<SignInManager>()
+			.AddEntityFrameworkStores<DbContext>()
+			.AddDefaultTokenProviders();
+
+			// Add application services.
+			services.AddTransient<PostManager>();
+			services.AddTransient<PageManager>();
+			services.AddTransient<CommentManager>();
+			services.AddTransient<CategoryManager>();
+			services.AddTransient<TagsManager>();
+			services.AddTransient<SettingManager>();
+			services.AddTransient<IEmailSender, EmailSender>();
+			//services.AddTransient<ISmsSender, AuthMessageSender>();
+			services.AddTransient<BlogMLImporter>();
+			services.AddTransient<BlogMLExporter>();
+			services.AddTransient<CaptchaService>();
+			services.AddTransient<IFileService, LocalFileService>();
+
+			services.AddScoped<DbInitializer>();
+
+			services.AddMemoryCache();
+			services.AddSession();
+
+			services.AddMvc()
+				.SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+			services.AddResponseCaching();
+			services.Configure<GzipCompressionProviderOptions>(options => options.Level = System.IO.Compression.CompressionLevel.Optimal);
+			services.AddResponseCompression(options =>
+			{
+				options.EnableForHttps = false;
+			});
+
+			services.AddAuthorization(options =>
+			{
+				// options.AddPolicy("Admin", p => p.AddRequirements(new AdminRequirement()));
+			});
+
+			services.AddScoped(s => s.GetService<SettingManager>().LoadSetting<BasicSettings>());
+			services.AddScoped(s => s.GetService<SettingManager>().LoadSetting<AdvancedSettings>());
+			services.AddScoped(s => s.GetService<SettingManager>().LoadSetting<EmailSettings>());
+			services.AddScoped(s => s.GetService<SettingManager>().LoadSetting<CommentsSettings>());
+			services.AddScoped(s => s.GetService<SettingManager>().LoadSetting<FeedSettings>());
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, DbInitializer dbInitializer)
+		{
+			// init database 
+			dbInitializer.Initialize();
+
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseDatabaseErrorPage();
+			}
+			else
+			{
+				//app.UseStatusCodePagesWithRedirects("~/error/{0}");
+				app.UseExceptionHandler("/error");
+
+				app.UseHsts();
+			}
+
+			app.UseResponseCompression();
+			app.UseResponseCaching();
+
+			app.UseStaticFiles(new StaticFileOptions()
+			{
+				OnPrepareResponse = context =>
+				{
+					// Cache static file for 1 year
+					if (!string.IsNullOrEmpty(context.Context.Request.Query["v"]))
+					{
+						context.Context.Response.Headers.Add("cache-control", new[] { "public, max-age=31536000" });
+						context.Context.Response.Headers.Add("Expires", new[] { DateTime.UtcNow.AddYears(1).ToString("R") }); // Format RFC1123
+					}
+				}
+			});
+
+			if (UseHttps)
+			{
+				app.UseHttpsRedirection();
+			}
+
+			app.UseCookiePolicy();
+
+			app.UseImageAxdMiddleware();
+
+			app.UseSession();
+
+			app.UseForwardedHeaders(new ForwardedHeadersOptions
+			{
+				ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+			});
+
+			app.UseAuthentication();
+
+			app.UseMvc(RegisterRoutes);
+		}
+
+		private void RegisterRoutes(IRouteBuilder routes)
+		{
+			routes.MapAreaRoute(
+					name: "admin",
+					areaName: "admin",
+					template: "admin/{controller=Default}/{action=Index}/{id?}");
+
+			routes.MapRoute(
+				name: RouteNames.LogIn,
+				template: "account/login",
+				defaults: new { controller = "account", action = "login" });
+			routes.MapRoute(
+				name: RouteNames.Logout,
+				template: "account/logout",
+				defaults: new { controller = "account", action = "logout" });
+			routes.MapRoute(
+				name: RouteNames.Register,
+				template: "account/register",
+				defaults: new { controller = "account", action = "register" });
+			routes.MapRoute(
+				name: RouteNames.ChangePassword,
+				template: "account/changePassword",
+				defaults: new { controller = "account", action = "changepassword" });
 
 
-            services.AddMemoryCache();
-            services.AddSession();
+			routes.MapRoute(
+				name: RouteNames.Home,
+				template: "",
+				defaults: new { controller = "Home", action = "Index" });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+			routes.MapRoute(
+			   name: RouteNames.Archive,
+			   template: "archive",
+			   defaults: new { controller = "home", action = "archive" });
 
-            // services.AddResponseCaching();
-            services.Configure<GzipCompressionProviderOptions>(options => options.Level = System.IO.Compression.CompressionLevel.Optimal);
-            services.AddResponseCompression(options =>
-            {
-                options.EnableForHttps = true;
-            });
+			routes.MapRoute(
+				name: RouteNames.Post,
+				template: "post/{slug}",
+				defaults: new { controller = "home", action = "post" });
 
-            services.AddAuthorization(options =>
-            {
-                // options.AddPolicy("Admin", p => p.AddRequirements(new AdminRequirement()));
-            });
+			routes.MapRoute(
+				name: RouteNames.Page,
+				template: "page/{slug}",
+				defaults: new { controller = "home", action = "page" });
 
-            services.AddScoped(s => s.GetService<SettingManager>().LoadSetting<BasicSettings>());
-            services.AddScoped(s => s.GetService<SettingManager>().LoadSetting<AdvancedSettings>());
-            services.AddScoped(s => s.GetService<SettingManager>().LoadSetting<EmailSettings>());
-            services.AddScoped(s => s.GetService<SettingManager>().LoadSetting<CommentsSettings>());
-            services.AddScoped(s => s.GetService<SettingManager>().LoadSetting<FeedSettings>());
-        }
+			routes.MapRoute(
+			  name: RouteNames.Author,
+			  template: "author/{username}",
+			  defaults: new { controller = "home", action = "index" });
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, DbInitializer dbInitializer)
-        {
-            // init database 
-            dbInitializer.Initialize();
+			routes.MapRoute(
+			   name: RouteNames.Tags,
+			   template: "tag/{name}",
+			   defaults: new { controller = "home", action = "index" });
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/error");
-                app.UseHsts();
-            }
+			routes.MapRoute(
+			   name: RouteNames.Category,
+			   template: "category/{name}",
+			   defaults: new { controller = "home", action = "index" });
 
-            //app.UseStatusCodePagesWithRedirects("~/error/{0}");
+			routes.MapRoute(
+			   name: RouteNames.Monthlist,
+			   template: "{year:int}/{month:range(1,12)}",
+			   defaults: new { controller = "home", action = "index" });
 
-            app.UseResponseCompression();
+			routes.MapRoute(
+				name: RouteNames.Search,
+				template: "search",
+				defaults: new { controller = "home", action = "index" });
 
-            // app.UseResponseCaching();
+			routes.MapRoute(
+				name: RouteNames.CommentForm,
+				template: "comment/{slug}",
+				defaults: new { controller = "home", action = "AddComment" });
 
-            app.UseStaticFiles(new StaticFileOptions()
-            {
-                OnPrepareResponse = context =>
-                {
-                    // Cache static file for 1 year
-                    if (!string.IsNullOrEmpty(context.Context.Request.Query["v"]))
-                    {
-                        context.Context.Response.Headers.Add("cache-control", new[] { "public,max-age=31536000" });
-                        context.Context.Response.Headers.Add("Expires", new[] { DateTime.UtcNow.AddYears(1).ToString("R") }); // Format RFC1123
-                    }
-                }
-            });
+			routes.MapRoute(
+				name: RouteNames.NotFound,
+				template: "notfound",
+				defaults: new { controller = "home", action = "notfound" });
 
-            app.UseHttpsRedirection();
-            app.UseCookiePolicy();
+			// default 
+			routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+		}
 
-            app.UseImageAxdMiddleware();
-
-            app.UseSession();
-
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-            });
-
-            app.UseAuthentication();
-
-            app.UseMvc(RegisterRoutes);
-        }
-
-        private void RegisterRoutes(IRouteBuilder routes)
-        {
-            routes.MapAreaRoute(
-                    name: "admin",
-                    areaName: "admin",
-                    template: "admin/{controller=Default}/{action=Index}/{id?}");
-
-            routes.MapRoute(
-                name: RouteNames.LogIn,
-                template: "account/login",
-                defaults: new { controller = "account", action = "login" });
-            routes.MapRoute(
-                name: RouteNames.Logout,
-                template: "account/logout",
-                defaults: new { controller = "account", action = "logout" });
-            routes.MapRoute(
-                name: RouteNames.Register,
-                template: "account/register",
-                defaults: new { controller = "account", action = "register" });
-            routes.MapRoute(
-                name: RouteNames.ChangePassword,
-                template: "account/changePassword",
-                defaults: new { controller = "account", action = "changepassword" });
-
-
-            routes.MapRoute(
-                name: RouteNames.Home,
-                template: "",
-                defaults: new { controller = "Home", action = "Index" });
-
-            routes.MapRoute(
-               name: RouteNames.Archive,
-               template: "archive",
-               defaults: new { controller = "home", action = "archive" });
-
-            routes.MapRoute(
-                name: RouteNames.Post,
-                template: "post/{slug}",
-                defaults: new { controller = "home", action = "post" });
-
-            routes.MapRoute(
-                name: RouteNames.Page,
-                template: "page/{slug}",
-                defaults: new { controller = "home", action = "page" });
-
-            routes.MapRoute(
-              name: RouteNames.Author,
-              template: "author/{username}",
-              defaults: new { controller = "home", action = "index" });
-
-            routes.MapRoute(
-               name: RouteNames.Tags,
-               template: "tag/{name}",
-               defaults: new { controller = "home", action = "index" });
-
-            routes.MapRoute(
-               name: RouteNames.Category,
-               template: "category/{name}",
-               defaults: new { controller = "home", action = "index" });
-
-            routes.MapRoute(
-               name: RouteNames.Monthlist,
-               template: "{year:int}/{month:range(1,12)}",
-               defaults: new { controller = "home", action = "index" });
-
-            routes.MapRoute(
-                name: RouteNames.Search,
-                template: "search",
-                defaults: new { controller = "home", action = "index" });
-
-            routes.MapRoute(
-                name: RouteNames.CommentForm,
-                template: "comment/{slug}",
-                defaults: new { controller = "home", action = "AddComment" });
-
-            routes.MapRoute(
-                name: RouteNames.NotFound,
-                template: "notfound",
-                defaults: new { controller = "home", action = "notfound" });
-
-            // default 
-            routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-        }
-
-    }
+	}
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
-# Passingwind.Blog 2017 
-基于asp.net core 2.1, microsoft.identity.core 和 entity framework core 。
-地址：[http://blog.wuliping.cn](http://blog.wuliping.cn)
+# Passingwind.Blog
+
+基于asp.net core 2.2, microsoft.identity.core 和 entity framework core 。
+地址：[http://blog.wuliping.cn](https://blog.wuliping.cn)
